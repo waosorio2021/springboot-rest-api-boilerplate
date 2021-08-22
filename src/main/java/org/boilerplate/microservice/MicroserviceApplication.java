414d8df4ec71edfb9c6c9eef477d8b839bf51988
Wilson Andrés Osorio Muñoz <wilsonandresosorio2018@gmail.com>
2021-08-22 18:52:57 -0500

    Refactorizando la entryPoint Class del microservicio.

--- src/main/java/org/boilerplate/microservice/MicroserviceApplication.java
+++ src/main/java/org/boilerplate/microservice/MicroserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroservicioApplication {
+public class MicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroservicioApplication.class, args);
+		SpringApplication.run(MicroserviceApplication.class, args);
 	}
 
 }
